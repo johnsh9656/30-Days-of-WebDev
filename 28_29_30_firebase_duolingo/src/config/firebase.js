420e6211e5ef3124b1b1fedc971274bf445b99a8
johnsh9656 <harrisonjohns1219@gmail.com>
2024-08-07 01:00:31 -0400

    Day 28, 29, 30 - Duolingo Clone /w Firebase, React

--- 28_29_30_firebase_duolingo/src/config/firebase.js
+++ 28_29_30_firebase_duolingo/src/config/firebase.js
@@ -4,13 +4,7 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDwNhlxmFlVPW1Xm0VqveW6zOVffP8Dn7o",
-  authDomain: "duolingo-clone-cd694.firebaseapp.com",
-  projectId: "duolingo-clone-cd694",
-  storageBucket: "duolingo-clone-cd694.appspot.com",
-  messagingSenderId: "729518657884",
-  appId: "1:729518657884:web:1765eb43da9c31ba2ff199",
-  measurementId: "G-VZG3HVZ6RZ"
+  // firebase info here
 };
 
 const app = initializeApp(firebaseConfig);
